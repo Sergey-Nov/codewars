07b43111ffcca62a747b1a801136c10f6960bd34
Sergey_Nov <takalik19941994@gmail.com>
2022-09-18 00:00:30 +0300

    main.js

---
+++ script.js
@@ -0,0 +1,70 @@
+/* Given a month as an integer from 1 to 12, return to which quarter
+ of the year it belongs as an integer number.For example: month 2 (February), 
+ is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November),
+  is part of the fourth quarter. */
+  const quarterOf = (month) => {
+    // Your code here
+    if (month<=3){
+      return 1
+    }else if(month>3&&month<=6){
+      return 2
+    }else if(month>6&&month<=9){
+      return 3
+    }else{
+      return 4
+    }
+    
+  }
+
+
+
+  /* Complete the function that accepts a string parameter, 
+  and reverses each word in the string. All spaces in the string should be retained. */
+
+  function reverseWords(str) {
+    var res = str.split(' ').map((word)=>{
+      return word.split('').reverse().join('')}).join(' ');
+    return res;
+  }
+
+
+/* Task
+
+Sum all the numbers of a given array ( cq. list ), 
+except the highest and the lowest element ( by value, not by index! ).
+The highest or lowest element respectively is a single element at each edge, 
+even if there are more than one with the same value. */
+  function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
+  }
+
+  /* Create a function that takes an integer as an argument 
+  and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+function evenOrAdd(num){
+    return res = num%2==0 ? "Even" : "Odd"
+}
+
+
+/* You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+ */
+
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :) 
+      return parseInt(inputString.slice(0))
+    }
