88f983f98155f1808d3186cb36bf1c9720c994f7
Sergey_Nov <takalik19941994@gmail.com>
2022-09-20 20:52:58 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -67,4 +67,38 @@ const HIGH_TEMPERATURES = {
   // Only change code above this line
   /* ================================================== */
 
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
   
+  // Only change code below this line
+    
+  const {today :highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES; 
+  
+  // Only change code above this line
+  /* =================================================== */
+
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+  const {today: {low: lowToday,high: highToday}} = LOCAL_FORECAST  
+  
+  
+  // Only change code above this line
+
+  /* ===================================================== */
+
+  const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a,b,...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
