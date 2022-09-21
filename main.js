7cc9a18dddfb57e240e3269b9fc12ae5455c7b7b
Sergey_Nov <takalik19941994@gmail.com>
2022-09-21 20:44:27 +0300

    new taskin main.js

--- main.js
+++ main.js
@@ -133,3 +133,16 @@ function makeList(arr) {
 }
 
 const failuresList = makeList(result.failure);
+
+/* ================================================= */
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
+
+/* =================================================== */
