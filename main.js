75911c650abfb63aa6e6b518a19e79fd580a97d1
Sergey_Nov <takalik19941994@gmail.com>
2022-09-21 20:36:27 +0300

    new taskin main.js

--- main.js
+++ main.js
@@ -115,3 +115,21 @@ const stats = {
 // Only change code below this line
 const half = ({max,min}) => (max + min) / 2.0; 
 // Only change code above this line
+
+/* =========================================================== */
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  
+    const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`);
+  
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
