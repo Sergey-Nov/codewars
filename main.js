a00c13bf05c4e2be41b57b2c78442d89eb366636
Sergey_Nov <takalik19941994@gmail.com>
2022-09-22 19:41:01 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -148,3 +148,16 @@ const createPerson = (name, age, gender) => {
 /* =================================================== */
 const makeServerRequest=new Promise((resolve,reject)=>{})
 /* =================================================== */
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    // Change this line
+    resolve("We got the data")
+  } else {  
+    // Change this line
+    reject("Data not received")
+  }
+});
+/* ==================================================== */
