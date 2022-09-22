b231d81f0caaf2ba8b1e3aedec72ba95a697243f
Sergey_Nov <takalik19941994@gmail.com>
2022-09-22 20:01:42 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -198,3 +198,7 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 /* ========================================================== */
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+/* ============================================================== */
