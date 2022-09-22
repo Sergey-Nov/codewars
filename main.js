e369b1402b1c46fb54800799b5e5152d24ed8d49
Sergey_Nov <takalik19941994@gmail.com>
2022-09-22 20:10:12 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -206,3 +206,9 @@ let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
 /* ================================================================== */
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+/* ==================================================================== */
+
+
