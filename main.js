287eb8876a4f17c0d75fd77e96fb48229f7ab26b
Sergey_Nov <takalik19941994@gmail.com>
2022-09-24 21:26:55 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -224,3 +224,7 @@ let result = twinkleStar.match(starRegex); // Change this line
 
 /* =============================================================== */
 
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+/* ================================================================ */
