1c0350947160242bb606ea80dbb055bb1f075369
Sergey_Nov <takalik19941994@gmail.com>
2022-09-24 23:32:24 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -243,3 +243,9 @@ let myRegex = /[^aeiou0-9]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
 
 /* ================================================================ */
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
+
+/* ================================================================ */
+
