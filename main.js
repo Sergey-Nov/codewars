50327384c762ef48f338834aa08e08ca0c042302
Sergey_Nov <takalik19941994@gmail.com>
2022-09-24 23:57:59 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -257,3 +257,9 @@ let result = chewieQuote.match(chewieRegex);
 
 /* ================================================================ */
 
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<h*?1>/; // Change this line
+let result = text.match(myRegex);
+
+/* =================================================================== */
+
