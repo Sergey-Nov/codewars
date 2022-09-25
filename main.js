67a4c4ab032ec415173e4a162fd2d5d812d6d65d
Sergey_Nov <takalik19941994@gmail.com>
2022-09-25 22:00:33 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -327,5 +327,11 @@ let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g; // Change this line
 let result = sample.match(countWhiteSpace);
 
-/* =============================================================== */
+/* ===========================Поиск символов кроме пробелов==================================== */
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
+
+/* =================================================================== */
 
