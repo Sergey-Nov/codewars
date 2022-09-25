71747e8053d243a0e94db750b6e9fb36e48b8eb3
Sergey_Nov <takalik19941994@gmail.com>
2022-09-25 21:21:10 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -279,3 +279,9 @@ let result = lastRegex.test(caboose);
 
 /* =================================================================== */
 
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/gi; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
+
+/* ================================================================= */
+
