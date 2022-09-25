3ac933aefa3166dc5d2d959aa6a74bc5e27e929b
Sergey_Nov <takalik19941994@gmail.com>
2022-09-25 21:17:09 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -273,3 +273,9 @@ let result = calRegex.test(rickyAndCal);
 
 /* ================================================================== */
 
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
+
+/* =================================================================== */
+
