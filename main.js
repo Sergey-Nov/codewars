2856fccaa40f41cb322ebd864e7205bca2092b6b
Sergey_Nov <takalik19941994@gmail.com>
2022-09-25 21:43:57 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -295,5 +295,11 @@ let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/g; // Change this line
 let result = movieName.match(numRegex).length;
 
-/* =========================================================================== */
+/* ==========================Поиск символов кроме чисел[^0-9]======================================== */
 
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length;
+
+
+/* ================================================================= */
