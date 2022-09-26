37294b5e50fc4bd26dbbf216105ad6fad7ccf6ba
Sergey_Nov <takalik19941994@gmail.com>
2022-09-26 21:16:37 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -430,4 +430,8 @@ console.log(`Sum of array values is: ${arraySum}`);
 
 /* ========================================================== */
 
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+/* ========================================================= */
 
