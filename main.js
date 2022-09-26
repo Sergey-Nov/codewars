9b6dca02c34950341e86e699ce218928627c00c5
Sergey_Nov <takalik19941994@gmail.com>
2022-09-26 21:24:11 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -435,3 +435,17 @@ console.log(innerHtml);
 
 /* ========================================================= */
 
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+/* ========================================================== */
+
