a82b0706c7009ac63bf8dfbea84d6e83c2ce68f0
Sergey_Nov <takalik19941994@gmail.com>
2022-09-26 21:04:05 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -408,4 +408,13 @@ console.clear()
 
 /*============================================================ */
 
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
+
+/* =========================================================== */
+
 
