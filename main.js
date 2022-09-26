f7dc4fb2627c451314008d92b0051285b9ff00c5
Sergey_Nov <takalik19941994@gmail.com>
2022-09-26 21:14:00 +0300

    new task in main.js

--- main.js
+++ main.js
@@ -424,4 +424,10 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 
 /* =========================================================== */
 
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current)
+console.log(`Sum of array values is: ${arraySum}`);
+
+/* ========================================================== */
+
 
